576d1b70c84e04402ddce7ed5a881a9e976db813
Talal1904 <talaldona@gmail.com>
2024-11-22 03:56:03 +0500

    frontend signup page added

--- backend/forumProject/forumProject/settings.py
+++ backend/forumProject/forumProject/settings.py
@@ -84,9 +84,9 @@ WSGI_APPLICATION = 'forumProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'userTry',  # Replace with your schema name
+        'NAME': 'db',  # Replace with your schema name
         'USER': 'root',     # Your MySQL username
-        'PASSWORD': 'Hond@city2014',  # Your MySQL password
+        'PASSWORD': '1234',  # Your MySQL password
         'HOST': 'localhost',
         'PORT': '3306',
     }
--- frontend/frontend/src/components/SignIn.js
+++ frontend/frontend/src/components/SignIn.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './SignIn.css'
 
 const SignIn = ({ setLoggedInUser }) => {
     const [username, setUsername] = useState('');
@@ -17,26 +18,48 @@ const SignIn = ({ setLoggedInUser }) => {
     };
 
     return (
-        <div>
-            <h1>Sign In</h1>
-            <form onSubmit={handleSignIn}>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Sign In</button>
-            </form>
-            <p>{message}</p>
-        </div>
+        <div class="wrapper">
+<div class="title">
+   Login Form
+</div>
+<form onSubmit={handleSignIn}>
+   <div class="field">
+      <input 
+      type="text"
+      placeholder="username" 
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+       
+       />
+      
+      
+   </div>
+   <div class="field">
+      <input 
+      type="password"
+      placeholder= 'password'
+      value={password}
+      onChange={(e)=>setPassword(e.target.value)}
+      />
+      
+   </div>
+   <div class="content">
+      <div class="pass-link">
+         <a href="#">Forgot password?</a>
+      </div>
+   </div>
+   <div class="field">
+      <input type="submit" value="Login" />
+   
+   </div>
+   <div class="signup-link">
+      Not a member? <a href="#">Signup now</a>
+   </div>
+</form>
+</div>
     );
 };
 
 export default SignIn;
+
+
