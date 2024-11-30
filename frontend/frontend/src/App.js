fd67369917c8c155080f3ebe754509d3d9b4ee69
MShaheerMalik77 <157911864+MShaheerMalik77@users.noreply.github.com>
2024-12-01 01:25:54 +0500

    App.js now contains the routes for all of the pages
    Admin Dashboard and Other Profile do not yet have any direct links to them as they depend on the admin's sign in / the person's profile being clicked leading to their page.

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -4,10 +4,13 @@ import Register from './components/Register';
 import SignIn from './components/SignIn';
 import Dashboard from './components/Dashboard'; 
 import AnonDashboard from './components/anonDashboard';
+import AdminDashboard from './components/AdminDashboard';
 import UpdateUser from './components/updateUser';
 import PostForm from './components/Postform';
 import Home from './components/Home';
 import ViewPosts from './components/viewPost';
+import SelfProfile from './components/SelfProfile';
+import OtherProfile from './components/OtherProfile'; 
 import axios from 'axios';
 
 const App = () => {
@@ -39,11 +42,14 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/anonDashboard" element={<AnonDashboard />} />
+                <Route path="/AdminDashboard" element={<AdminDashboard />} />
                 <Route path="/signin" element={<SignIn setLoggedInUser={setLoggedInUser} />} />
                 <Route path="/dashboard" element={<Dashboard loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />} />
                 <Route path="/updateUser" element={<UpdateUser />} />
                 <Route path="/postForm" element={<PostForm loggedInUser={loggedInUser} />} />
                 <Route path="/viewPosts" element={<ViewPosts />} />
+                <Route path="/selfprofile" element={<SelfProfile />} /> 
+                <Route path="/OtherProfile" element={<OtherProfile />} /> 
             </Routes>
         </Router>
     );
